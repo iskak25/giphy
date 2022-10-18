baa1d2c38759583eecf6c1608cbb6b1bf7265d35
iskak25 <iskak2512@gmail.com>
2022-10-18 14:59:27 +0600

    end

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let APIKEY = "GA902ccSNaYV5Dbygm91rcsa5K1luNyp";
-// you will need to get your own API KEY
-// https://developers.giphy.com/dashboard/
+
 document.addEventListener("DOMContentLoaded", init);
 function init() {
   document.getElementById("btnSearch").addEventListener("click", (ev) => {
-    ev.preventDefault(); //to stop the page reload
+    ev.preventDefault();
     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
     let str = document.getElementById("search").value.trim();
     url = url.concat(str);
@@ -12,7 +11,6 @@ function init() {
     fetch(url)
       .then((response) => response.json())
       .then((content) => {
-        //  data, pagination, meta
         console.log(content.data);
         console.log("META", content.meta);
         let fig = document.createElement("figure");
